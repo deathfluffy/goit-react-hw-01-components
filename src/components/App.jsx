846cd5d73640048c370b9fd5f14b7ea88ab22265
deathfluffy <mrandreu454@gmail.com>
2023-12-11 16:04:32 +0200

    ++++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
 import { Friends } from './friends/Friens';
 import { TransactionHistory } from './transactions/Transactions';
-import user from './data/user.json';
-import data from './data/data.json';
-import transactions from './data/transactions.json';
-import friends from './data/friends.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import transactions from '../data/transactions.json';
+import friends from '../data/friends.json';
 
 export const App = () => {
   return (
